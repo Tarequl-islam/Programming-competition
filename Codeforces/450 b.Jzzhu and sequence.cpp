ad82a7f2b7f715bed575d16872b2dfe864a819d5
tarequl-islam <itareq38@gmail.com>
2020-10-08 01:31:18 +0600

    ladder day26

---
+++ Codeforces/450 b.Jzzhu and sequence.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i, n) for (i = 0; i < n; i++)
+#define ll long long
+#define si(x) scanf("%d", &x)
+#define sl(x) scanf("%lld", &x)
+#define ss(s) scanf("%s", s)
+#define pi(x) printf("%d\n", x)
+#define pl(x) printf("%lld\n", x)
+#define ps(s) printf("%s\n", s)
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+int main(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll n, m, a, b, c, i, j, mx=0, mn=2222222;
+    ll ar[10];
+    sl(a);
+    sl(b);
+    sl(n);
+    ar[1]=a; ar[2]=b; ar[3]=b-a; ar[4]=-a; ar[5]=-b; ar[6]=ar[0]=a-b;
+    c = ar[n%6];
+    cout << (c % 1000000007 + 1000000007) % 1000000007 << endl;
+    return 0;
+}
--- Exp/sound test.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<dos.h>
- main()
-{
-sound(100);
-delay(300);
-return 0;
-}
