b8c4d54d8b31b0d2f43890632f5c64daa4727646
tarequl-islam <itareq38@gmail.com>
2020-10-12 14:27:36 +0600

    800

---
+++ Codeforces/104 a.Blackjack.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i, n) for (i = 0; i < n; i++)
+#define ll long long
+#define si(x) scanf("%d", &x)
+#define sl(x) scanf("%lld", &x)
+#define ss(s) scanf("%s", s)
+#define pi(x) printf("%d\n", x)
+#define pl(x) printf("%lld\n", x)
+#define ps(s) printf("%s\n", s)
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+bool cmp(const pii &left, const pii &right){
+    return left.first > right.first || (left.first == right.first && left.second < right.second);
+}
+int main(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int n, m, a, b, c, i, j, mx=0, mn=2222222;
+    int ar[100005];
+    si(n);
+    if (n==20){
+        cout<<15<<endl;
+    }
+    else if(n-10 >=1 && n-10 <= 11){
+        cout<<4<<endl;
+        return 0;
+    }
+    else cout<<0<<endl;
+    return 0;
+}
