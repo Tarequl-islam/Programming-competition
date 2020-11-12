a975bcf6ebbbfa672ac9f2e5efc14310cd2331db
tarequl-islam <itareq38@gmail.com>
2020-11-12 10:15:31 +0600

    virtual contest-1445

---
+++ Codeforces/1445 a.namee.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i, n) for (i = 0; i < n; i++)
+#define ll long long
+#define si(x) scanf("%d", &x)
+#define sl(x) scanf("%lld", &x)
+#define ss(s) scanf("%s", s)
+#define pi(x) printf("%d\n", x)
+#define pl(x) printf("%lld\n", x)
+#define ps(s) printf("%s\n", s)
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+bool cmp(const pii &left, const pii &right){
+    return left.first > right.first || (left.first == right.first && left.second < right.second);
+}
+int main(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int t, cs = 1;
+    cin >> t;
+    while (t--){
+        int n, m,x, a, b, c, i, j, mx = 0, mn = 2222222;
+        int ar[105];
+        int br[100];
+        cin>>n>>x;
+        for (i = 0; i < n; i++){
+            si(ar[i]);
+        }
+        for (i = 0; i < n; i++){
+            si(br[i]);
+        }
+        sort(ar, ar+n);
+        sort(br, br+n);
+        for ( i = 0; i < n; i++){
+            if(ar[i]+br[n-(i+1)] > x){
+                mx=1;
+                break;
+            }
+        }
+        if(mx==1) cout<<"No\n";
+        else cout<<"Yes\n";
+        if(t>1) cin.ignore();
+        
+    }
+    return 0;
+}
---
+++ Codeforces/1445 c.Division.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i, n) for (i = 0; i < n; i++)
+#define ll long long
+#define si(x) scanf("%d", &x)
+#define sl(x) scanf("%lld", &x)
+#define ss(s) scanf("%s", s)
+#define pi(x) printf("%d\n", x)
+#define pl(x) printf("%lld\n", x)
+#define ps(s) printf("%s\n", s)
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+bool cmp(const pii &left, const pii &right){
+    return left.first > right.first || (left.first == right.first && left.second < right.second);
+}
+int main(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int t, cs = 1;
+    cin >> t;
+    while (t--){
+        ll n, m, a, b, c,d, i, j, mx = 0, mn = 2222222;
+        cin>>a>>b;
+        if(a%b!=0) cout<<a<<endl;
+        else{
+            d = sqrt(a);
+            for ( i = 2; i <= d; i++){
+                c = (a/i);
+                if(c*i == a && (a/i)%b!=0){
+                    cout<<a/i<<endl;
+                    break;
+                }
+            }
+        }
+    }
+    return 0;
+}
