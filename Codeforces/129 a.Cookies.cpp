bcb6070051530505c295abee78a58739a4670d5f
tarequl-islam <itareq38@gmail.com>
2021-01-07 08:49:53 +0600

    ladder12 day-1

---
+++ Codeforces/129 a.Cookies.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i, n) for (i = 0; i < n; i++)
+#define ll long long
+#define si(x) scanf("%d", &x)
+#define sl(x) scanf("%lld", &x)
+#define ss(s) scanf("%s", s)
+#define pi(x) printf("%d\n", x)
+#define pl(x) printf("%lld\n", x)
+#define ps(s) printf("%s\n", s)
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 2*acos(0.0)
+#define M 998244353
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+ll GCD(ll a, ll b) { return (a % b) == 0 ? b : GCD(b, a % b); }
+ll mod(ll x) { return ((x % M + M) % M); }
+bool cmp(const pii &left, const pii &right){
+    return left.first > right.first || (left.first == right.first && left.second < right.second);
+}
+int main(){
+
+        ll n, m, a, b, c,e=0,o=0, i, j, k, mx = 0, mn = 1e18;
+        ll ar[100005];
+        cin>>n;
+        for (i = 0; i < n; i++){
+            sl(ar[i]);
+            mx+=ar[i];
+            if(ar[i]%2==0) e++;
+            else o++;
+
+        }
+        if(mx%2!=0){
+            cout<<o<<endl;
+        }
+        else cout<<e<<endl;
+    return 0;
+}
