39720fda43b7ec2809a81f725aa68f5700bdfa50
tarequl-islam <itareq38@gmail.com>
2021-03-12 00:59:15 +0600

    ladder13 day2

---
+++ Codeforces/152 a.marks.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i, n) for (i = 0; i < n; i++)
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define ll long long
+#define si(x) scanf("%d", &x)
+#define sl(x) scanf("%lld", &x)
+#define ss(s) scanf("%s", s)
+#define pi(x) printf("%d\n", x)
+#define pl(x) printf("%lld\n", x)
+#define ps(s) printf("%s\n", s)
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define CLR(a, b) memset(a, b, sizeof(a))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 2*acos(0.0)
+#define M 998244353
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+const int MOD = 1000 * 1000 * 1000 + 7;
+const int MAXN = 10 * 1000 * 1000 + 10, MAXV = 4;
+ll GCD(ll a, ll b) { return (a % b) == 0 ? b : GCD(b, a % b); }
+ll mod(ll x) { return ((x % M + M) % M); }
+bool cmp(const pii &left, const pii &right){
+    return left.first > right.first || (left.first == right.first && left.second < right.second);
+}
+
+int main(){
+    int n, m, a, b, c, i, j, k, mx = 0, mn = 1e18;
+    cin>>n>>m;
+    string s[105];
+    int ar[105]={0};
+    for(i=0; i<n; i++){
+        cin>>s[i];
+    }
+    for(i=0; i<m; i++){
+        for(j=0; j<n; j++){
+            if(s[j][i]-'0'> ar[i])
+                ar[i]= s[j][i]-'0';
+        }
+    }
+    for(i=0; i<n; i++){
+        for(j=0; j<m; j++){
+            if(s[i][j]-'0'>=ar[j]){
+                mx++;
+                break;
+            }
+        }
+    }
+    cout<<mx<<endl;
+    return 0;
+}
